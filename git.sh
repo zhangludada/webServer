bf9aaa561302e94b1edbb6218cd3a5ef1ce1abc6
zhangludada <zhangludada@hotmail.com>
2025-03-26 18:44:20 +0800

    my test commit

---
+++ git.sh
@@ -0,0 +1,7 @@
+echo "# webServer" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:zhangludada/webServer.git
+git push -u origin main
