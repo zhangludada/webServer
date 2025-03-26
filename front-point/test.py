a0c83933b43c93dadfcea9c99847f67a5a26051c
zhangludada <zhangludada@hotmail.com>
2025-03-26 19:03:44 +0800

    webServer first commit

---
+++ front-point/test.py
@@ -0,0 +1,7 @@
+ls=['manual',"projects",'blogs',"apps",'signin']
+
+import random
+
+r=random.choice(ls)
+print(r)
+
--- git.sh
+++ git.sh
@@ -1,7 +1,3 @@
-echo "# webServer" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
 git remote add origin git@github.com:zhangludada/webServer.git
+git branch -M main
 git push -u origin main
